16da35d161cdd82693c3ad629e03815ee31e2c80
aanno <aanno@users.noreply.github.com>
2025-01-19 16:34:07 +0100

    try to proxy port 8443 as well

--- aio-on-fcos/scripts/aio-stop.sh
+++ aio-on-fcos/scripts/aio-stop.sh
@@ -8,3 +8,12 @@ source .env
 export DOCKER DOCKER_SOCKET DOCKER_HOST AIO_DOMAIN SKIP_DOMAIN_VALIDATION MY_IPV4_ADDR
 
 $DOCKER compose -p aio down
+
+sleep 2
+
+$DOCKER rm -f \
+  nextcloud-aio-apache \
+  nextcloud-aio-notify-push \
+  nextcloud-aio-nextcloud \
+  nextcloud-aio-database \
+  nextcloud-aio-redis
