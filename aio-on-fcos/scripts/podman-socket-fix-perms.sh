442d2addd81c5ce341fdfbdc5c33ecbba7fd805e
aanno <aanno@users.noreply.github.com>
2025-02-10 12:49:00 +0000

    podman 5.3.1 comes with a new way to create docker API sockets

---
+++ aio-on-fcos/scripts/podman-socket-fix-perms.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+# www-data in container (id=33)
+MY_UID=720928
+MY_GID=720928
+
+export DOCKER_SOCKET=/run/user/$UID/podman/podman.sock
+export DOCKER_HOST=unix://$DOCKER_SOCKET
+echo "export DOCKER_SOCKET=$DOCKER_SOCKET"
+echo "export DOCKER_HOST=$DOCKER_HOST"
+
+# sudo chown -R $MY_UID:$MY_GID /run/user/$UID/podman
+chgrp $MY_GID $DOCKER_SOCKET
+chmod g+rw o-rwx $DOCKER_SOCKET
+
+podman system connection list
+
+curl -H "Content-Type: application/json" \
+	--unix-socket /run/user/$UID/podman/podman.sock \
+    http://localhost/_ping
+echo ""
+
+ls -l $DOCKER_SOCKET
